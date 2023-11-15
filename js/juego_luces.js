bc958f7c109c4398acef0a5e03fce491f14258de
diego <diego0112@hotmail.com>
2023-11-15 13:25:45 +0100

    Generamos tablero con JS y metodo para luces Ran.

--- js/juego_luces.js
+++ js/juego_luces.js
@@ -1,112 +1,34 @@
-// Obtener referencias a elementos HTML
-const board = document.querySelector('.board');
-const attemptsSpan = document.getElementById('attempts');
-const timerSpan = document.getElementById('timer');
-const selectButton = document.getElementById('select');
-const customRowsInput = document.getElementById('customRows');
-const customColsInput = document.getElementById('customcols');
-const customLightsInput = document.getElementById('customlights');
+//VARIABLES GLOBALES
+let filas = 5;
+let columnas = 6;
+let lucesIniciales = 10;
+let intentos = 0;
+let tiempo = 0;
+let juegoTerminado = false;
 
-// Variables para el juego
-let numRows = 5;
-let numCols = 6;
-let numLights = 10;
-let attempts = 0;
-let timer;
-let lightsRemaining = numLights;
+//FUNCION PARA CREAR EL TABLERO CON BOTONES
+function crearTablero(){
+    const tablero = document.getElementById("tablero");
+    tablero.innerHTML = "";
 
-// Función para inicializar el juego
-function initGame() {
-  // Generar el tablero
-  generateBoard();
-  
-  // Iniciar el temporizador
-  startTimer();
-  
-  // Actualizar el contador de intentos
-  attemptsSpan.textContent = attempts;
-}
-
-// Función para generar el tablero con celdas
-function generateBoard() {
-  board.innerHTML = '';
-  for (let i = 0; i < numRows; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < numCols; j++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      cell.dataset.row = i;
-      cell.dataset.col = j;
-      cell.addEventListener('click', handleCellClick);
-      row.appendChild(cell);
+//RECORREMOS CON BUCLES FOR TANTO FILAS COMO COLUMNAS
+    for (let i = 0; i < filas; i++) {
+        for (let j = 0; j < columnas; j++) {
+        const boton = document.createElement("button"); //CREAMOS LOS BOTONES
+        boton.className = "boton";
+        boton.addEventListener("click", () =>{
+            if (!juegoTerminado) { //SI ES VERDADERO YA QUE LO DEFINIMOS EN FALSE
+                cambiarEstadoVentana(i, j);
+                actualizarIntentos();
+                verificarFindeJuego();
+        }
+    });
+    /*AGREGA BOTONES Y LINEAS DE SALTO DE LINEA COMO ELEMENTOS
+    HIJO AL ELEMENTO TABLERO*/
+    tablero.appendChild(boton);  
+        }
+        tablero.appendChild(document.createElement("br"));
     }
-    board.appendChild(row);
+        //METODO PARA GENERAR LUCES ALEATORIAS
+        generarLucesAleatorias();
   }
-  // Lógica para encender celdas aleatorias
-  for (let i = 0; i < numLights; i++) {
-    const randomRow = Math.floor(Math.random() * numRows);
-    const randomCol = Math.floor(Math.random() * numCols);
-    const cell = document.querySelector(`.cell[data-row="${randomRow}"][data-col="${randomCol}"]`);
-    cell.classList.add('active');
-  }
-}
-
-// Función para empezar el temporizador
-function startTimer() {
-  let seconds = 0;
-  timer = setInterval(function() {
-    seconds++;
-    timerSpan.textContent = seconds;
-  }, 1000);
-}
-
-// Función para manejar el clic en una celda
-function handleCellClick(event) {
-  const cell = event.target;
-  // Si la celda está encendida, apágala; si está apagada, enciéndela
-  if (cell.classList.contains('active')) {
-    cell.classList.remove('active');
-    lightsRemaining--;
-  } else {
-    cell.classList.add('active');
-    lightsRemaining++;
-  }
-  // Incrementar el contador de intentos
-  attempts++;
-  attemptsSpan.textContent = attempts;
-  // Verificar si todas las luces están encendidas
-  if (lightsRemaining === 0) {
-    clearInterval(timer);
-    alert('¡Ganaste!');
-  }
-}
-
-// Función para configurar el juego personalizado
-function setCustomGame() {
-  const customRows = parseInt(customRowsInput.value);
-  const customCols = parseInt(customColsInput.value);
-  const customLights = parseInt(customLightsInput.value);
-
-  // Verificar que los valores sean válidos
-  if (Number.isInteger(customRows) && Number.isInteger(customCols) && Number.isInteger(customLights) && customLights < customRows * customCols) {
-    numRows = customRows;
-    numCols = customCols;
-    numLights = customLights;
-    lightsRemaining = numLights;
-    attempts = 0;
-    clearInterval(timer);
-    timerSpan.textContent = 0;
-    attemptsSpan.textContent = 0;
-    generateBoard();
-  } else {
-    alert('Por favor, ingresa valores válidos para Filas, Columnas y Luces.');
-  }
-}
-
-// Eventos para el clic en el botón Seleccionar y el radio button Personalizado
-selectButton.addEventListener('click', initGame);
-document.querySelector('input[name="difficulty"][value="custom"]').addEventListener('click', setCustomGame);
-
-// Iniciar el juego cuando se cargue la página
-window.addEventListener('load', initGame);
